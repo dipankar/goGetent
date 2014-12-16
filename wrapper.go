2997f054e5326f45486d6cc578f1a07b8a0ec0df
Dipankar Sarkar <dipankarsarkar@gmail.com>
2014-12-16 08:41:08 +0530

    Added exec wrapper to getent

--- wrapper.go
+++ wrapper.go
@@ -1,8 +1,14 @@
 package goGetent
 
 import (
+	"os/exec"
+	"fmt"
 )
 
-func main() {
-	out, err := exec.Command("sh","-c","ls").Output()
+// Make a call to getent and get an output
+func get_ent(arg0 string,arg1 string) {
+    app := "getent"
+    cmd := exec.Command(app, arg0, arg1)
+    stdout, err := cmd.Output()
+    return string(stdout),err
 }
